5d8912fad2784e00a8c7b8c630e5eecd02ad81a3
Matthias Valvekens <matthias@mvalvekens.be>
2022-01-29 01:07:11 +0100

    Expose profile API at top level.

--- certomancer/__init__.py
+++ certomancer/__init__.py
@@ -1,13 +1,13 @@
 from .registry import (
     CertomancerConfig, PKIArchitecture,
-    ExtensionPlugin, extension_plugin_registry,
-    ServicePlugin, service_plugin_registry,
+    ExtensionPlugin, CertProfilePlugin, extension_plugin_registry,
+    ServicePlugin, service_plugin_registry, cert_profile_plugin_registry,
     AttributePlugin, attr_plugin_registry,
 )
 
 __all__ = [
     'PKIArchitecture', 'CertomancerConfig',
-    'ExtensionPlugin', 'ServicePlugin', 'AttributePlugin',
+    'ExtensionPlugin', 'ServicePlugin', 'AttributePlugin', 'CertProfilePlugin',
     'extension_plugin_registry', 'service_plugin_registry',
-    'attr_plugin_registry',
+    'attr_plugin_registry', 'cert_profile_plugin_registry'
 ]
--- certomancer/registry/__init__.py
+++ certomancer/registry/__init__.py
@@ -11,8 +11,10 @@ from .keys import KeySet, KeySets
 from .issued.cert import CertificateSpec
 from .issued.attr_cert import AttributeCertificateSpec
 from .plugin_api import (
-    PluginServiceRequestError, ExtensionPlugin, ServicePlugin, AttributePlugin,
+    PluginServiceRequestError,
+    ExtensionPlugin, ServicePlugin, AttributePlugin, CertProfilePlugin,
     extension_plugin_registry, service_plugin_registry, attr_plugin_registry,
+    cert_profile_plugin_registry
 )
 
 
@@ -23,8 +25,8 @@ __all__ = [
     'KeySet', 'KeySets',
     'KeyLabel', 'CertLabel', 'EntityLabel', 'ServiceLabel',
     'PluginLabel', 'ArchLabel',
-    'ExtensionPlugin', 'ServicePlugin', 'AttributePlugin',
+    'ExtensionPlugin', 'ServicePlugin', 'AttributePlugin', 'CertProfilePlugin',
     'PluginServiceRequestError',
     'extension_plugin_registry', 'service_plugin_registry',
-    'attr_plugin_registry',
+    'attr_plugin_registry', 'cert_profile_plugin_registry'
 ]
